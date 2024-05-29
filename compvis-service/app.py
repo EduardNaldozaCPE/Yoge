ec13fea3e12e7afe4b7b79f17777a26fb17aa4c3
Eduard Naldoza <80999519+EduardNaldozaCPE@users.noreply.github.com>
2024-05-29 17:23:31 +0300

    refactor(websocket): dump frame byte data

--- compvis-service/app.py
+++ compvis-service/app.py
@@ -1,7 +1,9 @@
 import websockets
 import asyncio
 import threading
+import time
 from PoseEstimationService import PoseEstimationService
+import numpy as np
 
 IpAddress = '127.0.0.1'
 port = 8081
@@ -20,16 +22,32 @@ async def echo(websocket, path):
 # TODO --   Get the frame data from PoseEstimationService.runVideo() method
 #           Follow Pipeline / Stack:
 #               Request -> SocketController -> PoseEstimationService -> SocketController -> Response
+
+# Initialise the service
 poseEstimationService = PoseEstimationService()
-async def processVideo():
-    # frame_data = poseEstimationService.runVideo()
-    # await websocket.send(data)
-    pass
+# Create it's own separate thread since it has an endless loop
+video_thread = threading.Thread(target=poseEstimationService.runVideo)
+video_thread.start()
 
 
+async def processVideo(websocket):
+    counter = 0
+    while counter < 10:
+        frame_data = poseEstimationService.getFrameData()
+        # print(f'ooga {counter} {frame_data}')
+        if frame_data:
+            decoded_frame = str(len(frame_data))
+            with open("./tests/byte-dump.txt", "w") as text_file:
+                print("writing byte dump")
+                text_file.write(decoded_frame)
+        # await websocket.send(frame_data)
+        counter += 1
+        time.sleep(1)
+    poseEstimationService.stopVideo()
 
-start_server = websockets.serve(echo, IpAddress, port)
-# start_server = websockets.serve(processVideo, IpAddress, port)
+
+# start_server = websockets.serve(echo, IpAddress, port)
+start_server = websockets.serve(processVideo, IpAddress, port)
 asyncio.get_event_loop().run_until_complete(start_server)
 print(f"WebSocket server started on ws://{IpAddress}:{port}")
 asyncio.get_event_loop().run_forever()
