f3e6dd53596b82da584d6bcaf28f9db86ce7b752
Eduard Naldoza <80999519+EduardNaldozaCPE@users.noreply.github.com>
2024-06-03 17:45:02 +0300

    feat: send data continuously & handle KeyboardInterrupt

--- compvis-service/app.py
+++ compvis-service/app.py
@@ -1,9 +1,7 @@
 import websockets
 import asyncio
 import threading
-import time
 from PoseEstimationService import PoseEstimationService
-import numpy as np
 
 ADDRESS = '127.0.0.1'
 PORT = 8081
@@ -17,22 +15,31 @@ video_thread.start()
 
 
 async def processVideo(websocket):
-    counter = 0
-    while counter < 10:
+    connected = True
+    while connected:
         frame_data = poseEstimationService.getFrameData()
-        # print(f'ooga {counter} {frame_data}')
         if frame_data is not None:
             decoded_frame = str(len(frame_data))
-            with open("./tests/byte-dump.txt", "w") as text_file:
-                print("writing byte dump")
-                text_file.write(decoded_frame)
-        # TODO await websocket.send(frame_data)
-        counter += 1
-        time.sleep(1)
+            await websocket.send(decoded_frame)
+            await asyncio.sleep(0.001)
+            print(f"Sent: {decoded_frame}")
+            
+        if not connected:
+            break
+
     poseEstimationService.stopVideo()
 
 
+print("Running app.py")
 start_server = websockets.serve(processVideo, ADDRESS, PORT)
-asyncio.get_event_loop().run_until_complete(start_server)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_server)
 print(f"WebSocket server started on ws://{ADDRESS}:{PORT}")
-asyncio.get_event_loop().run_forever()
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    print("Stopped by user")
+finally:
+    poseEstimationService.stopVideo()
+    loop.stop()
+    loop.close()
