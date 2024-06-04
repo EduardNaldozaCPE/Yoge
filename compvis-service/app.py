c3d84753995c5ccea168d70653f8c4041dd55df5
Eduard Naldoza <80999519+EduardNaldozaCPE@users.noreply.github.com>
2024-06-04 21:51:41 +0300

    refactor to use pipes.

--- compvis-service/app.py
+++ compvis-service/app.py
@@ -1,52 +1,45 @@
 import os
-import websockets
-import asyncio
+import time
 import threading
 from PoseEstimationService import PoseEstimationService
 
-ADDRESS = '127.0.0.1'
-PORT = 8081
-DISCONNECT_STR = "!DISCONNECT"
-
-# Initialise the service
-# TODO -- Take in userId, sequenceId, sessionId
-poseEstimationService = PoseEstimationService()
+PIPE_NAME = r'\\.\pipe\frame_pipe'
 
 
-async def processVideo(websocket):
-    poseEstimationService.setSessionData()
-    connected = True 
+def main():
+    # Initialise the service
+    try:
+        # TODO -- Take in userId, sequenceId, sessionId
+        poseEstimationService = PoseEstimationService()
+        poseEstimationService.setSessionData()
+    except:
+        return
+    
+    isRunning = True
+
     # Create a separate thread for runVideo since it has an endless loop
     video_thread = threading.Thread(target=poseEstimationService.runVideo)
     video_thread.start()
 
-    while connected:
+    while isRunning:
         frame_data = poseEstimationService.getFrameData()
         if frame_data is not None:
             try:
-                await websocket.send(frame_data)
-            except:
-                print("Socket Disconnected. Stopping video loop")
-                connected = False
-            finally:
-                await asyncio.sleep(0.001)
+                # send frame_data to pipe
+                # print(str(len(frame_data)))
+                time.sleep(0.1)
+                pass
+            except KeyboardInterrupt:
+                print("Program Interrupted. Stopping Video Loop...")
+                isRunning = False
             
-        if not connected:
+        if not isRunning:
             break
 
     poseEstimationService.stopVideo()
     video_thread.join()
 
-os.system("cls")
-start_server = websockets.serve(processVideo, ADDRESS, PORT)
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_server)
-print(f"WebSocket server started on ws://{ADDRESS}:{PORT}")
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    print("Stopped by user")
-finally:
-    poseEstimationService.stopVideo()
-    loop.stop()
-    loop.close()
+
+if __name__ == "__main__":
+    os.system("cls")
+    main()
