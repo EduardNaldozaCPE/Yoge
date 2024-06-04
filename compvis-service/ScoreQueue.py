80b4e2ff20ac6c9d08f3f6db60322cfbf8e13a7d
Eduard Naldoza <80999519+EduardNaldozaCPE@users.noreply.github.com>
2024-06-04 17:02:36 +0300

    feat: Create ScoreQueue to process score list

---
+++ compvis-service/ScoreQueue.py
@@ -0,0 +1,42 @@
+import queue as q
+import sqlite3
+
+DBPATH = "../score-data/yoge.db"
+
+# Processes the PoseLandmarkerResult Queue to insert into the database   
+class ScoreQueue:
+    # Initialise scores queue and sqlite connection
+    def __init__(self, userId, sessionId, sequenceId = 0):
+        self.userId = userId
+        self.sessionId = sessionId
+        self.sequenceId = sequenceId
+
+        self.scores = q.Queue()
+        self.con = sqlite3.connect(DBPATH)
+        self.cur = self.con.cursor()
+        
+        # Create a new row in the session table once ScoreQueue object is created
+        self.cur.execute(f"""
+            INSERT INTO session (userId, sequenceId) VALUES ({self.userId}, {self.sequenceId});
+            """)
+
+
+    # Puts a new score into the queue
+    def addScore(self, score):
+        self.scores.put(score)
+
+
+    # Processes the scores queue one-by-one
+    def processScores(self):
+        # Starts processing the scores if they're the queue is not empty
+        while True:
+            if self.scores.empty(): continue
+
+            lastScore = self.scores.get()
+            
+            # TODO -- INSERT THIS SCORE DATA INTO THE DATABASE
+            print(len(lastScore))
+            # self.cur.execute(f"""
+            #     INSERT INTO score VALUES 
+            #                  ({})
+            #     """)
