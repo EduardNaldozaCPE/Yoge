84359dd6d615fdc1c9b069b23c5466bfbfa530b4
Eduard Naldoza <80999519+EduardNaldozaCPE@users.noreply.github.com>
2024-07-05 20:40:24 +0100

    refactor(fe): Remove FrameConsumer
    Note: Functions with callbacks don't work well with es6 classes.
    net.on('data') runs on event triggers, and since it is already looping, there is no need to save a currentFrame property.

--- yoge-electron/src/session/session.js
+++ yoge-electron/src/session/session.js
@@ -1,22 +1,56 @@
-const cv = require('./opencv/opencv.js');
-const FrameConsumer = require("../frame_consumer/FrameConsumer.js");
+// const cv = require('../opencv/opencv.js');
+const net = require('node:net');
 
-const fc = new FrameConsumer("framePipe");
-var currentFrame = Buffer("", "utf-8");
-
-fc.connect();
-try {
-    currentFrame = fc.getFrame(); // TODO -- Loop this.
-} catch (error) {
-    console.log(error);
-    fc.disconnect();
-} finally {
-    delete fc;
+const BUFFERSIZE = 1048576;
+const PIPEDIR = "\\\\.\\pipe\\framePipe";
+const connectionListener = () => {
+    console.log("Successfully Connected.");
 }
 
-console.log("Current Frame: ");
-console.log(currentFrame);
-
 // TODO:
-//  Install opencv to decode bytes to string (Refer to https://www.youtube.com/watch?v=qexy4Ph66JE&list=LL&index=1&t=509s) 
-//  Use ./framebytes as a test (via fs streaming)
+//  - Run main() on one thread, use currentFrame to store the current frame.
+// Other:
+//  - Handle ENOENT when named pipe isn't created.
+//  - Install opencv to decode bytes to string (Refer to https://www.youtube.com/watch?v=qexy4Ph66JE&list=LL&index=1&t=509s) 
+
+var currentFrame = Buffer.alloc(BUFFERSIZE, "\x01", 'utf-8');
+const liveFeed = document.getElementById("live-feed");
+if (liveFeed) {
+    setInterval(() => {
+        //  TODO -- Run interval on updating the live feed (use current frame every 30ms).
+    }, 1000);
+}
+
+function main() {
+    // 1. Connect to named pipe using 'node:net'
+    console.log(`Connecting to named pipe: ${PIPEDIR}`);
+    try {
+        var client = net.createConnection( `${PIPEDIR}`, connectionListener);
+    } catch (error) {
+        console.log(`Encountered an error while connecting: \n${error}`);
+    }
+
+    
+    client.on('data', (data) => {
+        // Handle on data.
+        console.log(data.length);
+        currentFrame = data;
+    });
+    
+
+    client.on('end', () => {
+        // Handle on end.
+        console.log("Disconnecting from the named pipe.");
+    });
+    
+    function disconnect() {
+        console.log("Disconnecting from the named pipe.");
+        client.end((err) => {
+            if (err) {
+                console.log(`Encountered error while attempting to disconnect from the named pipe: ${err}`);
+            }
+        });
+    }
+}
+
+main();
