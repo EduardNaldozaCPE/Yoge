86de4c9f82ff329d520ea288335e332e30b7d475
Eduard Naldoza <80999519+EduardNaldozaCPE@users.noreply.github.com>
2024-07-11 21:48:58 +0100

    add todo

--- yoge-electron/src/pages/session/session.js
+++ yoge-electron/src/pages/session/session.js
@@ -1,4 +1,6 @@
-// const liveFeed = document.getElementById('live-feed');
-// electronAPI.runConsumer();
+const liveFeed = document.getElementById('live-feed');
+electronAPI.runConsumer();
 
-// electronAPI.currentFrame((imgStr)=>{liveFeed.src = imgStr;});
+// TODO -- Re-open Named pipe on exit. To fix unsuccessful connections. (ie when camera is off)
+//  Create a signal to stop connection from the client.
+electronAPI.currentFrame((imgStr)=>{liveFeed.src = imgStr;});
